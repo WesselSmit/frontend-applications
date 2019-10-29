d63e83585d0f2c8db9fbc17e622bf4fb2819bd8a
Wessel Smit <wrsmit00@gmail.com>
2019-10-29 21:20:18 +0100

    component merge

--- src/components/item.vue
+++ src/components/item.vue
@@ -23,26 +23,31 @@ article {
   width: auto;
   display: inline-block;
   cursor: pointer;
+  box-shadow: rgba(0, 0, 0, 0.16) 1px 8px 8px;
 }
 
-.left > div > div > article {
+.left > div > article {
   border-radius: 5px 0px 5px 5px;
 }
 
-.right > div > div > article {
+.right > div > article {
   border-radius: 0px 5px 5px 5px;
 }
 
 article > h2 {
   font-size: 20px;
-  text-align: center;
   color: #d6d7d9;
   font-weight: 400;
+  background-color: #474c56;
+  text-align: center;
+  padding-top: 3px;
 }
 
 article > img {
   max-height: 200px;
   width: auto;
-  max-width: 100%;
+  max-width: 40vw;
+  display: block;
+  margin: auto;
 }
 </style>
--- src/components/itemListR.vue
+++ src/components/itemListR.vue
@@ -1,78 +1,80 @@
-<template>
+// <template>
+  <!--   
   <div>
+
     <div v-for="(item, index) in items" v-bind:key="item.id" v-bind:id="'item' + index">
+
       <item v-bind:item="item" />
     </div>
-  </div>
+  </div>-->
 </template>
 
-<script>
-import axios from "axios";
-import item from "./item.vue";
+// <script>
+// import axios from "axios";
+// import item from "./item.vue";
 
-export default {
-  name: "itemList",
-  components: {
-    item
-  },
-  props: {
-    item: null
-  },
-  data() {
-    return {
-      items: []
-    };
-  },
-  created() {
-    const url =
-        "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-23/sparql",
-      query = `
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX dc: <http://purl.org/dc/elements/1.1/>
-        PREFIX dct: <http://purl.org/dc/terms/>
-        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        PREFIX edm: <http://www.europeana.eu/schemas/edm/>
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+// export default {
+//   name: "itemList",
+//   components: {
+//     item
+//   },
+//   data() {
+//     return {
+//       items: []
+//     };
+//   },
+//   created() {
+//     const url =
+//         "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-23/sparql",
+//       query = `
+//         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+//         PREFIX dc: <http://purl.org/dc/elements/1.1/>
+//         PREFIX dct: <http://purl.org/dc/terms/>
+//         PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+//         PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+//         PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+//         PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-        SELECT ?cho ?img ?title ?cat WHERE {
-          #Categorie
-          	<https://hdl.handle.net/20.500.11840/termmaster2816> skos:* ?catURI .
-          	?cho edm:isRelatedTo ?catURI .
-            ?catURI skos:prefLabel ?cat .
-          #Image
-            ?cho edm:isShownBy ?img .
-          #Title
-            ?cho dc:title ?title .
+//         SELECT ?cho ?img ?title ?cat WHERE {
+//           #Categorie
+//           	<https://hdl.handle.net/20.500.11840/termmaster2816> skos:* ?catURI .
+//           	?cho edm:isRelatedTo ?catURI .
+//             ?catURI skos:prefLabel ?cat .
+//           #Image
+//             ?cho edm:isShownBy ?img .
+//           #Title
+//             ?cho dc:title ?title .
 
-          FILTER langMatches(lang(?title), "eng")
-        } LIMIT 2
-      `;
-    axios
-      .get(url + "?query=" + encodeURIComponent(query) + "&format=json")
-      .then(res => (this.items = res.data.results.bindings))
-      .then(res => console.log(res))
-      .then(res => {
-        //Unavailable images have '-Extra' in their URL, so we renavigate the src to the unavailable image (URL without the '-Extra')
-        this.items.forEach(function(item) {
-          item.img.value = item.img.value.replace("http", "https");
-          if (item.img.value.includes("-Extra")) {
-            item.img.value = item.img.value.replace("-Extra", "");
-          }
-        });
-      })
-      .catch(err => console.log(err));
-  }
-};
+//           FILTER langMatches(lang(?title), "eng")
+//         } LIMIT 2
+//       `;
+//     axios
+//       .get(url + "?query=" + encodeURIComponent(query) + "&format=json")
+//       .then(res => (this.items = res.data.results.bindings))
+//       .then(res => console.log("right", res))
+//       .then(res => {
+//         //Unavailable images have '-Extra' in their URL, so we renavigate the src to the unavailable image (URL without the '-Extra')
+//         this.items.forEach(function(item) {
+//           item.img.value = item.img.value.replace("http", "https");
+//           if (item.img.value.includes("-Extra")) {
+//             item.img.value = item.img.value.replace("-Extra", "");
+//           }
+//         });
+//       })
+//       .catch(err => console.log(err));
+//   }
+// };
+//
+//
 </script>
 
 <style scoped>
-div:first-of-type {
+/* div:first-of-type {
   display: flex;
   flex-direction: column;
 }
 
 .right > div > div > article {
   align-self: flex-start;
-}
+} */
 </style>
